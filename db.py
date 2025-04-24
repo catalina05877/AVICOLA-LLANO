a0f27a5cfa8164d584b8c74f17312b25321f0c8b
catalina agudelo <agudelocatalina89@gmail.com>
2025-04-23 22:13:50 -0500

    third commit

--- db.py
+++ db.py
@@ -2,11 +2,11 @@
 from pymongo import MongoClient
 
 # Conexión a MongoDB Atlas o local
-MONGO_URI = "mongodb://localhost:27017"  
+MONGO_URI = "mongodb+srv://myAtlasDBUser:qwX9pqzhveRmwD8T@avicoladb.rkhdtqs.mongodb.net/?retryWrites=true&w=majority&appName=avicoladb"  
 client = MongoClient(MONGO_URI)
 
 # Selección de la base de datos
-db = client['avicola_llano_grande']
+db = client['avicoladb']
 
 # Colecciones (tablas en NoSQL)
 stock_collection = db['stock']
